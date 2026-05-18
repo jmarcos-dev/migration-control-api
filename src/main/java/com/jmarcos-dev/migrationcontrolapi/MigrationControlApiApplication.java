69dd6387313600283abc860a3d79bc38dfcde274
João Marcos <79798116+jmarcos-dev@users.noreply.github.com>
2026-05-18 18:03:56 +0000

    adicionado gradlew, docker-compose e rc

---
+++ src/main/java/com/jmarcos-dev/migrationcontrolapi/MigrationControlApiApplication.java
@@ -0,0 +1,12 @@
+package com.seuusuario.migrationcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationControlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigrationControlApiApplication.class, args);
+    }
+}
