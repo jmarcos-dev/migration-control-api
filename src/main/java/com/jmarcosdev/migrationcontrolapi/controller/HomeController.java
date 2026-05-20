c9e4c6e2543e71614ce3943424a484fa24becaef
João Marcos <joaomarcoscprofissional@gmail.com>
2026-05-19 22:40:46 -0300

    Alterado nome do caminho + adicionado controller

---
+++ src/main/java/com/jmarcosdev/migrationcontrolapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jmarcosdev.migrationcontrolapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "API rodando corretamente!";
+    }
+}
